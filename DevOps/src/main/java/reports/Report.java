084814bf82c4e9b62cfde48c17fd46bf73669785
Thomas van Otterloo <vanotterloo.thomas@gmail.com>
2024-03-21 04:45:33 +0100

    Testing

---
+++ DevOps/src/main/java/reports/Report.java
@@ -0,0 +1,10 @@
+package reports;
+
+import java.time.LocalDate;
+
+public class Report {
+    private String companyName;
+    private String reportName;
+    private double version;
+    private LocalDate date;
+}

c4637d03d2e4f8c47bdc5dcd619e82ff9fe6dda2
Thomas van Otterloo <vanotterloo.thomas@gmail.com>
2024-03-23 16:07:16 +0100

    Junk

--- DevOps/src/main/java/backlogItemState/Doing.java
+++ DevOps/src/main/java/backlogItemState/Doing.java
@@ -2,11 +2,7 @@ package backlogItemState;
 
 public class Doing implements BacklogItemState {
 
-<<<<<<< Updated upstream
-=======
 
-
->>>>>>> Stashed changes
     public BacklogItemState getCurrentState() {
         System.out.println("Backlog item is in the Doing state");
         return this;

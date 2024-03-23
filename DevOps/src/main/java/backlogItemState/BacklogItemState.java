c011000f4810008e463c9cce0e08824b549eafae
Spike Strous <spike.strous@gmail.com>
2024-03-23 13:24:30 +0100

    Refactor state pattern

--- DevOps/src/main/java/backlogItemState/BacklogItemState.java
+++ DevOps/src/main/java/backlogItemState/BacklogItemState.java
@@ -2,18 +2,6 @@ package backlogItemState;
 
 public interface BacklogItemState {
 
-    void toDo();
-
-    void doing();
-
-    void readyForTesting();
-
-    void testing();
-
-    void tested();
-
-    void done();
-
     BacklogItemState getCurrentState();
 
 
--- DevOps/src/main/java/backlogItemState/Tested.java
+++ DevOps/src/main/java/backlogItemState/Tested.java
@@ -2,34 +2,6 @@ package backlogItemState;
 
 public class Tested implements BacklogItemState {
 
-    public Tested() {
-        System.out.println("Backlog item has been changed to the Tested state");
-    }
-
-    public void toDo() {
-        System.out.println("Moving backlog item from Tested to the ToDo state");
-    }
-
-    public void doing() {
-        System.out.println("Moving backlog item from Tested to the Doing state");
-    }
-
-    public void readyForTesting() {
-        System.out.println("Moving backlog item from Tested to the Ready for Testing state");
-    }
-
-    public void testing() {
-        System.out.println("Moving backlog item from Tested to the Testing state");
-    }
-
-    public void tested() {
-        System.out.println("Backlog item is already in the Tested state");
-    }
-
-    public void done() {
-        System.out.println("Moving backlog item from Tested to the Done state");
-    }
-
     public BacklogItemState getCurrentState() {
         System.out.println("Backlog item is in the Tested state");
         return this;
